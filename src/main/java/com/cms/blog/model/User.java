e18362653b4bb0b502ca14e60f86de5b0956f328
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-12-15 17:29:09 +0900

    feat: Board, Reply 테이블 생성 연관 관계 (@ManyToOne, @OneToMany)

--- src/main/java/com/cms/blog/model/User.java
+++ src/main/java/com/cms/blog/model/User.java
@@ -11,7 +11,17 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //빌더 패턴!!
 //ORM -> Java(다른언어포함) Object -> 테이블로 매핑해주는 기술
 @Entity	//User 클래스가 MySQL에 테이블이 생성이 된다.
 public class User {
