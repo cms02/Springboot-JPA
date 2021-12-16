53168603033dcc992aad09d9774520c513e8b1c8
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-12-16 11:08:42 +0900

    feat: Select 적용 id로 select테스트 및 전체 select, paging 처리 관련

--- src/main/java/com/cms/blog/test/DummyControllerTest.java
+++ src/main/java/com/cms/blog/test/DummyControllerTest.java
@@ -1,6 +1,15 @@
 package com.cms.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +17,32 @@ import com.cms.blog.model.RoleType;
 import com.cms.blog.model.User;
 import com.cms.blog.repository.UserRepository;
 
+//html파일이 아니라 data를 리턴해주는 controller 
 @RestController
 public class DummyControllerTest {
 	
 	@Autowired //의존성 주입 (DI) 
 	private UserRepository userRepository;
+	
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한페이지당 2건의 데이터를 리턴받도록
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2,sort="id",direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	
+	
+	
+	
+	
 	
 	//http://localhost:8787/blog/dummy/join(요청)
 	//http의 body에 username, password, email 데이터를 가지고(요청)
@@ -36,6 +66,59 @@ public class DummyControllerTest {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return "회원가입이 완료되었습니다.";
+	}
+	
+	
+	//{id} 주소로 파라미터를 전달 받을 수 있음
+	// http://localhost:8787/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		//user/4를 찾았을 때, 데이터베이스에서 못찾았을시 user는 null
+		//return null -> 문제가됨..
+		//Optional로 User객체를 감싸서 가져옴 -> null인지 아닌지 판단해서 return !
+		
+//		User user = userRepository.findById(id).get(); //null이 나올 수가 없어! -> 위
+		
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {  //null 일 때 객체하나 만들어서 넣기!
+//
+//			@Override
+//			public User get() {
+//				// TODO Auto-generated method stub
+//				return new User();
+//			}
+//		});
+		
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {	// 비어있는 User 객체 생성 후 리턴
+//
+//			@Override
+//			public User get() {
+//				// TODO Auto-generated method stub
+//				return new User();
+//			}
+//		});
+		
+//		//람다식
+//		User user = userRepository.findById(id).orElseThrow(()->{
+//			return new IllegalArgumentException("해당 유저는 없습니다. id: " + id);
+//		});
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다.id: " + id);
+			}
+		});
+		
+		//요청 : 웹 브라우저
+		//user 객체 = 자바 오브젝트
+		//변환 (웹브라우저가 이해할 수 있는 데이터) -> json(Gson 라이브러리)
+		//스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		//만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		//user 오브젝트를 json으로 변환해서 브라우저에게 던져줌
+		
+		return user;
+		
 	}
 
 }
