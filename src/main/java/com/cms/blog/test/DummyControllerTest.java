0707a99c09a0147d5034ba6b6ae8def1cf266de9
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-12-16 13:28:47 +0900

    feat: update 테스트 영속성 컨텍스트와 더티체킹, @Transactional

--- src/main/java/com/cms/blog/test/DummyControllerTest.java
+++ src/main/java/com/cms/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cms.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cms.blog.model.RoleType;
@@ -23,6 +27,34 @@ public class DummyControllerTest {
 	
 	@Autowired //의존성 주입 (DI) 
 	private UserRepository userRepository;
+	
+	
+	//save함수는 id를 전달하지 않으면 insert를 해주고
+	//save함수는 id를 전달하고 해당 id에 대한 데이터가 있으면 update를 해주고
+	//save함수는 id를 전달하고 해당 id에 대한 데이터가 없으면 insert를 한다
+	
+	//email, password
+	
+	@Transactional	//함수 종료시에 자동 commit이 됨.
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json 데이터를 요청 -> Java Object(MessageConverter의 Jackson 라이브러리가 변환해서 받아줌 @RequestBody 필요!)
+		System.out.println("id: " 		+ id);
+		System.out.println("password: " + requestUser.getPassword());
+		System.out.println("email: " 	+ requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(requestUser);
+		
+		//더티 체킹
+		
+		return null;
+	}
 	
 	@GetMapping("/dummy/users")
 	public List<User> list(){
@@ -37,12 +69,6 @@ public class DummyControllerTest {
 		List<User> users = pagingUser.getContent();
 		return users;
 	}
-	
-	
-	
-	
-	
-	
 	
 	//http://localhost:8787/blog/dummy/join(요청)
 	//http의 body에 username, password, email 데이터를 가지고(요청)
