37bf17c9eb89c032c2242b8f5dea35942b9f0816
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-12-16 21:52:43 +0900

    feat: UserService, ResponseDto

---
+++ src/main/java/com/cms/blog/controller/api/UserApiController.java
@@ -0,0 +1,29 @@
+package com.cms.blog.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cms.blog.dto.ResponseDto;
+import com.cms.blog.model.RoleType;
+import com.cms.blog.model.User;
+import com.cms.blog.service.UserService;
+
+@RestController
+public class UserApiController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/api/user")
+	public ResponseDto<Integer> save(@RequestBody User user) {
+		System.out.println("UserApiController : Save 호출됨");
+		//실제로 DB에 insert를 하고 아래에서 return이 되면 됨.
+		user.setRole(RoleType.USER);
+		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+	}
+
+}
--- src/main/java/com/cms/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cms/blog/handler/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.cms.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cms.blog.dto.ResponseDto;
 
 //어디에서는 Exception이 발생했을 때 이곳으로 오도록
 @ControllerAdvice
@@ -10,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(value=IllegalArgumentException.class)
-	public String handleArgumentException(Exception e) {
-		return "<h1>" + e.getMessage() + "</h1>";
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
 	}
 
 }
