22e8d2c593922815c7e0facea69b03fade9deb3a
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-12-17 10:52:19 +0900

    feat: 로그인 기능 추가 전통적인 방식의 로그인 방법

--- src/main/java/com/cms/blog/repository/UserRepository.java
+++ src/main/java/com/cms/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cms.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cms.blog.model.User;
 
@@ -10,8 +11,12 @@ import com.cms.blog.model.User;
 //@Repository //생략 가능하다.
 
 public interface UserRepository extends JpaRepository<User, Integer>{ //User 테이블을 관리하며, PK 는 Integer
+	// JPA Naming 쿼리
+	// SELECT * FROM user WHERE username = ?1 AND password = ?2;
+	User findByUsernameAndPassword(String username, String password);
 	
-	
+	@Query(value="SELECT * FROM user WHERE username = ?1 AND password =?2", nativeQuery = true)
+	User login(String username, String password);
 	
 	
 
