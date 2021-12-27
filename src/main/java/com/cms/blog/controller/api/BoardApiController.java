ac476b19b21e3edf829b7eeab6ac1ad6bb3130f6
Chu MinSeok <cms02@r2ware.com>
2021-12-27 22:43:50 +0900

    feat: 댓글삭제

--- src/main/java/com/cms/blog/controller/api/BoardApiController.java
+++ src/main/java/com/cms/blog/controller/api/BoardApiController.java
@@ -58,6 +58,15 @@ public class BoardApiController {
 		
 	}
 	
+	@DeleteMapping("/api/board/{boardId}/reply/{replyId}")
+	public ResponseDto<Integer> replyDelete(@PathVariable int replyId){
+		
+		boardService.댓글삭제(replyId);
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+
+	}
+	
 	
 	
 	
